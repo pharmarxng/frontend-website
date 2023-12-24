eadf6019ca781463baccd44ae9179d9f35968465
ty-codes <titoadeoye00@gmail.com>
2023-12-24 05:42:24 -0800

    refactor: create modalgroup component

--- src/components/ModalGroup/index.tsx
+++ src/components/ModalGroup/index.tsx
@@ -1,6 +1,29 @@
+import { useModalContext } from 'context/modalContext';
+import Modal from 'react-modal';
+import { IModal } from 'utils/interfaces';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
 const ModalGroup = () => {
+    const { content, isOpenModal } = useModalContext() as IModal;
+
     return (
-        <>ModalGroup </>
+        <Modal
+            isOpen={isOpenModal}
+            style={customStyles}
+            className=''
+        >
+            {content}
+        </Modal>
     )
 }
 
