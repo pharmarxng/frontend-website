f00dacabb91a48008afe6d9fdefea59249b0a5e4
ty-codes <titoadeoye00@gmail.com>
2023-12-24 16:21:13 -0800

    feat: create adduser modal

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FocusEventHandler, HTMLInputTypeAttribute } from 'react';
+import { ChangeEvent, FocusEventHandler, HTMLInputTypeAttribute, ReactNode } from 'react';
 
 interface InputProps {
   type: HTMLInputTypeAttribute;
@@ -11,7 +11,9 @@ interface InputProps {
   onBlur?: FocusEventHandler<HTMLInputElement> | undefined;
   error?: string | boolean;
   checked?: boolean;
+  endDecorator?: JSX.Element;
 }
+
 
 const Input = ({
   type,
@@ -24,20 +26,27 @@ const Input = ({
   onBlur,
   error,
   checked,
+  endDecorator
 }: InputProps) => {
   return (
     <div>
-      <input
-        checked={checked}
-        id={id}
-        onBlur={onBlur}
-        type={type}
-        name={name}
-        onChange={changed}
-        value={value}
-        placeholder={placeholder ? placeholder : undefined}
-        className={`border-gray-200 border rounded-lg pl-4 py-3 w-full dark:text-black dark:bg-white  ${classDef}`}
-      />
+      <span className='relative'>
+        <input
+          checked={checked}
+          id={id}
+          onBlur={onBlur}
+          type={type}
+          name={name}
+          onChange={changed}
+          value={value}
+          placeholder={placeholder ? placeholder : undefined}
+          className={`border-gray-200 border rounded-lg pl-4 py-3 w-full dark:text-black dark:bg-white  ${classDef}`}
+        />
+        {endDecorator &&
+          <span className='absolute right-1 top-0 flex h-full'>{endDecorator}</span>
+        }
+      </span>
+
       <span className="text-red-700 text-xs lg:text-sm">{error}</span>
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,15 @@ import PageWrapper from "./PageWrapper";
 import DatePicker from "./DatePicker";
 import TopNavbar from "./TopNavbar";
 import ModalGroup from './ModalGroup';
+import Input from './Input';
+import Label from "./Label";
 
 export {
     SideNavbar,
     PageWrapper,
     DatePicker,
     TopNavbar,
-    ModalGroup
+    ModalGroup,
+    Label,
+    Input
 };
