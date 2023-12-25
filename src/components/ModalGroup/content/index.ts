f9a21dea1739d4823cce7ab92497d4d2015c51f6
ty-codes <titoadeoye00@gmail.com>
2023-12-25 01:34:05 -0800

    feat: create error message modal

--- src/components/ModalGroup/content/index.ts
+++ src/components/ModalGroup/content/index.ts
@@ -1,9 +1,11 @@
 import AddUser from './AddUser';
 import DeleteAdmin from './DeleteAdmin';
 import SuccessMessage from './SuccessMessage';
+import ErrorMessage from './ErrorMessage';
 
 export  {
     AddUser,
     DeleteAdmin, 
-    SuccessMessage
+    SuccessMessage,
+    ErrorMessage
 }
