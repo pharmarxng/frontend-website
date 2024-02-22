d89be4628290af19d89453a331dc2a33abaea523
sarmhi <sammiejay813@gmail.com>
2024-02-22 23:27:23 +0100

    added loading spinner for products

--- src/pages/ProductDescription.tsx
+++ src/pages/ProductDescription.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductByIdApi } from '../api/products';
 import Navbar from '../components/Navbar';
@@ -8,6 +8,7 @@ import ProductDescriptionBlock from '../components/blocks/ProductDescriptionBloc
 import { ProductState } from '../context/productContext';
 import RecentlyViewedProducts from '../components/blocks/RecentlyViewedProducts';
 import { AlertState } from '../context/alertContext';
+import { FadeLoader } from 'react-spinners';
 
 const ProductDescription = () => {
   const { productId } = useParams();
@@ -16,10 +17,28 @@ const ProductDescription = () => {
     productDispatch,
     productState: { product },
   } = ProductState();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       await getProductByIdApi(productDispatch, alertDispatch, productId!);
+      setLoading(false);
+      try {
+        setLoading(true);
+        const fetchedProduct = await getProductByIdApi(
+          productDispatch,
+          alertDispatch,
+          productId!
+        );
+        if (!fetchedProduct) {
+          throw new Error('Something went wrong');
+        }
+      } catch (error) {
+        console.error('Error fetching order:', error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -29,7 +48,20 @@ const ProductDescription = () => {
     <div className="bg-white min-h-screen">
       <Navbar />
       <PaddedWrapper>
-        {product && <ProductDescriptionBlock product={product} />}
+        {loading ? (
+          <div className="flex justify-center pt-[60px]">
+            <FadeLoader
+              color={'#2D547B'}
+              loading={loading}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        ) : product ? (
+          <ProductDescriptionBlock product={product} />
+        ) : (
+          <></>
+        )}
         <RecentlyViewedProducts currentLyViewedProductId={productId} />
         <Footer />
       </PaddedWrapper>
